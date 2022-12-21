e952fc7e82278ed738cee78f4f4280592a56af86
Chris <96923618+chrisdotz@users.noreply.github.com>
2022-12-20 21:10:18 -0600

    Create Index.py

---
+++ Index.py
@@ -0,0 +1,42 @@
+print("Hey there im going back to the right location")
+
+X = 9
+
+print(1+X)
+#remember that variables are case sensitive
+
+y = 10
+
+if y == 11:
+    print("Success")
+else:
+    print("Failure")
+
+#new variables dumped :)
+online = True
+printer = False
+#that is all for now
+
+print("\n"*2)
+#equal to 10 enters
+
+if printer and online is True:
+    print("Printing")
+else:
+    print("Sorry, but try to print again later")
+
+#new if / else
+if printer or online is False:
+    print("Sorry, but try to update your printer")
+else:
+    print("Thank you for your purchase of your new purchase. Happy Printing!! If there is any questions feel free to contact us! :)")
+
+z = 101
+
+t = input("Enter a number: ")
+t = int(t)
+
+if t == 11:
+    print ("Correct")
+else:
+    print("Failure")
